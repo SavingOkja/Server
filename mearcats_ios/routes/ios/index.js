806758f152f33161b03304a2563319374ae300f9
joowon <saven7788@gmail.com>
2017-10-28 23:41:48 +0900

    [ADD] PUT , GET /member/history

--- mearcats_ios/routes/ios/index.js
+++ mearcats_ios/routes/ios/index.js
@@ -66,6 +66,7 @@ router.route('/member')
     });
   });
 });
+
 router.route('/member/favorite')
 .put((req, res)=>{
 
@@ -85,16 +86,16 @@ router.route('/member/favorite')
 
     console.log(rows[0].favorite_list);
 
-	update(rows[0].favorite_list);
+    favoriteUpdate(rows[0].favorite_list);
   });
 
-  const update = (fv) => {
-	console.log(typeof fv);
-	fv = JSON.parse(fv);
-	console.log(typeof fv);
-    fv.push(req.body.company_id);
+  const favoriteUpdate = (fv) => {
+  	console.log(typeof fv);
+  	fv = JSON.parse(fv);
+  	console.log(typeof fv);
+      fv.push(req.body.company_id);
 
-let ar = JSON.stringify(fv);
+    let ar = JSON.stringify(fv);
     pool.query('update saving_okja.usr set favorite_list = ? where id = ?;',[ ar, req.body.user_id ], function( err, results ) {
 
       if (err){
@@ -156,9 +157,85 @@ let ar = JSON.stringify(fv);
 });
 
 router.route('/member/history')
-.post((req, res)=>{
+.put((req, res)=>{
+  pool.query('select history from saving_okja.usr where id = ?',[ req.body.id ], function( err, rows ) {
 
+    if (err){
+      console.log(err);
+      res.json({
+        result: false,
+        msg: "db 접속 에러",
+        qry: this.sql
+      });
+      return;
+    }
+    console.log(rows[0].history);
+    historyUpdate(rows[0].history);
+  });
 
+  const historyUpdate = (hs) => {
+    console.log(typeof hs);
+    hs = JSON.parse(hs);
+    console.log(typeof hs);
+    hs.push(req.body.company_id);
+
+    hs = JSON.stringify(hs);
+    pool.query('update saving_okja.usr set favorite_list = ? where id = ?;',[ hs, req.body.id ], function( err, results ) {
+
+      if (err){
+        console.log(err);
+        res.json({
+          result: false,
+          msg: "db 접속 에러",
+          qry: this.sql
+        });
+        return;
+      }
+      if(results.affectedRows == 1){
+        res.json({
+          result: true,
+          msg: "됬습니다",
+        });
+      }else{
+        res.json({
+          result: false,
+          msg: "update실패",
+        });
+
+      }
+
+    });
+  };
+
+})
+.get((req, res)=>{
+
+  pool.query('select history from saving_okja.usr where id=?',[req.query.id], function( err, rows ) {
+
+    if (err){
+      console.log(err);
+      res.json({
+        result: false,
+        msg: "db 접속 에러",
+        qry: this.sql
+      });
+      return;
+    }
+
+    if(rows[0].length !== 0){
+      res.status(200).json({
+        result: true,
+        msg: "history 입니다.",
+        data: rows[0].history
+      });
+    }else{
+      res.status(200).json({
+        result: true,
+        msg: "history가 없습니다",
+      });
+    }
+
+  });
 });
 
 
