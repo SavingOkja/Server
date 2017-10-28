b3c1d360d1faee8e1df57d0127f431fc16c73226
joowon <saven7788@gmail.com>
2017-10-28 11:33:35 +0900

    [ADD] db setting

--- mearcats_ios/routes/index.js
+++ mearcats_ios/routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express     = require('express');
+const Router      = new express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/**********************
+        MySQL 연동
+**********************/
+const mysql        = require('mysql');
+const db_config    = require('../config/db_config.json');
+
+//db connection 몇개 남았는 지 알려줘서 보내는 코드
+
+global.pool = mysql.createPool({
+    host : db_config.host,
+    port : db_config.port,
+    user : db_config.user,
+    password : db_config.password,
+    database : db_config.database,
+    connectionLimit : db_config.connectionLimit
 });
 
-module.exports = router;
+let LeftConnections = connectionLimit;
+pool.on('acquire', function (connection) {
+    LeftConnections--;
+    if( LeftConnections < 5 ){
+        console.log("DB Connections이 "+LeftConnections+"개 밖에 남지 않았습니다!");
+    }
+});
+
+pool.on('enqueue', function () {
+    console.log("DB Connections이 고갈됨");
+
+});
+
+pool.on('release', function (connection) {
+    LeftConnections++;
+});
+
+pool.getConnection(function(err, connection) {
+    if( err ){
+        console.log("error 처리",err);
+        return;
+    }
+
+    connection.ping(function (err) {
+        if (err) throw err;
+        console.log('Server responded to ping');
+    });
+});
+
+
+const ios = require('./ios');
+Router.use('/ios', ios);
+
+module.exports = exports = Router;
